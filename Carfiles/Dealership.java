be4ec4c1e64b1daa15f903a2d6c54930196bf205
Lovish Haider <156478115+CodExTitan@users.noreply.github.com>
2024-06-01 01:50:21 +0200

    Add files via upload

---
+++ Carfiles/Dealership.java
@@ -0,0 +1,15 @@
+package OopsLearnung.Carfiles;
+
+public class Dealership {
+    private Car[] cars;
+
+    public Dealership(Car[] cars) {
+        this.cars = new Car[cars.length];
+        for (int i = 0; i < cars.length; i++) {
+            this.cars[i] = new Car(cars[i]);
+
+        }
+
+    }
+}
+
